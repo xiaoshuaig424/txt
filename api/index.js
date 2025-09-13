f43b0276417c5c311688057a79556deec6a866ad
xiaoshuaig424 <struct777@outlook.com>
2025-09-13 13:54:55 +0800

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const url = "https://api.xygeng.cn/one";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const text = `${data.data.content} —— ${data.data.origin}`;
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.send(text);
+}
